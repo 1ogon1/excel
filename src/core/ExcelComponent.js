db802c36ce97348a8e1b0548f426029bee798387
Ruslan <konovalenkoruslan@gmail.com>
2020-10-31 19:35:01 +0200

    finish base framework functional

---
+++ src/core/ExcelComponent.js
@@ -0,0 +1,22 @@
+import {DOMListener} from '@core/DOMListener';
+
+export class ExcelComponent extends DOMListener {
+  // Возвращает шаблон компонента
+  constructor($root, options = {}) {
+    super($root, options.listeners)
+
+    this.name = options.name || ''
+  }
+
+  toHTML() {
+    return ''
+  }
+
+  init() {
+    this.initDOMListeners()
+  }
+
+  destroy() {
+    this.removeDOMListeners()
+  }
+}
